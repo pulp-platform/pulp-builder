5ecc1133723cbaea3e451f1037121319fc031efe
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-12-15 17:35:14 +0100

    Few fixes

--- configs/fpgas/pulpissimo/genesys2.sh
+++ configs/fpgas/pulpissimo/genesys2.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+scriptDir="$(dirname "$(dirname "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")")")"
+
+export PULP_SDK_HOME=$scriptDir
+
+source $PULP_SDK_HOME/pulp-runtime/configs/pulpissimo.sh
+source $PULP_SDK_HOME/configs/common.sh
 
 export PULP_CURRENT_CONFIG=pulpissimo@config_file=fpgas/pulpissimo/genesys2.json
 
-export PULP_CURRENT_CONFIG_ARGS=platform=fpga
+export PULPRUN_PLATFORM=fpga
+
+export io=uart
